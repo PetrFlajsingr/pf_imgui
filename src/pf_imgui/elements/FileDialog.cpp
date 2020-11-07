605f5bf373cb5d4dfdcc1a595e0cf4983f5cc6bd
petr <xflajs00@stud.fit.vutbr.cz>
2020-11-07 16:02:56 +0100

    add FileDialog

---
+++ src/pf_imgui/elements/FileDialog.cpp
@@ -0,0 +1,9 @@
+//
+// Created by petr on 11/7/20.
+//
+
+#include "FileDialog.h"
+
+namespace pf::ui::ig {
+
+}
---
+++ src/pf_imgui/elements/FileDialog.h
@@ -0,0 +1,14 @@
+//
+// Created by petr on 11/7/20.
+//
+
+#ifndef PF_IMGUI_ELEMENTS_FILEDIALOG_H
+#define PF_IMGUI_ELEMENTS_FILEDIALOG_H
+
+namespace pf::ui::ig {
+class FileDialog {
+};
+
+}
+
+#endif//PF_IMGUI_ELEMENTS_FILEDIALOG_H
