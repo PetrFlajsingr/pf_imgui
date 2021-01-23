0a2ec82de9448285027dfee0e33bd5a4d2948ece
petr <xflajs00@stud.fit.vutbr.cz>
2021-01-23 11:54:42 +0100

    add ItemElement

---
+++ src/pf_imgui/elements/interface/ItemElement.cpp
@@ -0,0 +1,7 @@
+//
+// Created by petr on 1/23/21.
+//
+
+#include "ItemElement.h"
+
+namespace pf::ui::ig {}
---
+++ src/pf_imgui/elements/interface/ItemElement.h
@@ -0,0 +1,20 @@
+//
+// Created by petr on 1/23/21.
+//
+
+#ifndef PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_INTERFACE_ITEMELEMENT_H
+#define PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_INTERFACE_ITEMELEMENT_H
+
+#include "Element.h"
+
+namespace pf::ui::ig {
+
+class ItemElement : public virtual Element {
+ public:
+
+ private:
+
+};
+
+}// namespace pf
+#endif//PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_INTERFACE_ITEMELEMENT_H
