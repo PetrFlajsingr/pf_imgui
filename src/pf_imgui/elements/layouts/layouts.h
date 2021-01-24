243b47315646d3ea16f419a3d572e69411f72690
petr <xflajs00@stud.fit.vutbr.cz>
2021-01-24 17:07:23 +0100

    add new layouts to layouts.h

--- src/pf_imgui/elements/layouts/layouts.h
+++ src/pf_imgui/elements/layouts/layouts.h
@@ -6,6 +6,8 @@
 #define PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_LAYOUTS_LAYOUTS_H
 
 #include "BoxLayout.h"
+#include "StackedLayout.h"
+#include "GridLayout.h"
 #include "Layout.h"
 
 #endif//PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_LAYOUTS_LAYOUTS_H
