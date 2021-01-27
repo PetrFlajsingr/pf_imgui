3cb4c14cbbb2f9dd65176e00b537231b4130b97b
petr <xflajs00@stud.fit.vutbr.cz>
2021-01-27 09:48:08 +0100

    fix getSizeIfCollapsed return value

--- src/pf_imgui/elements/layouts/ResizableLayout.cpp
+++ src/pf_imgui/elements/layouts/ResizableLayout.cpp
@@ -11,7 +11,7 @@ ResizableLayout::ResizableLayout(const std::string &elementName, const ImVec2 &s
     : Layout(elementName, allowCollapse, showBorder), Resizable(size) {}
 
 ImVec2 ResizableLayout::getSizeIfCollapsed() const {
-  return isCollapsed() ? getSize() : ImVec2{getSize().x, 10};
+  return isCollapsed() ? ImVec2{getSize().x, 10} : getSize();
 }
 
 }// namespace pf::ui::ig
