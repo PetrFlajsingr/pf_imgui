16712a4f6857c6eec7e0f403899ef04ddd4ee020
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-11 20:05:31 +0200

    add Table.cpp/h

---
+++ src/pf_imgui/elements/Table.cpp
@@ -0,0 +1,11 @@
+//
+// Created by petr on 4/11/21.
+//
+
+#include "Table.h"
+
+
+namespace pf::ui::ig {
+
+
+}
---
+++ src/pf_imgui/elements/Table.h
@@ -0,0 +1,12 @@
+//
+// Created by petr on 4/11/21.
+//
+
+#ifndef PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_TABLE_H
+#define PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_TABLE_H
+
+namespace pf::ui::ig {
+class Table {};
+}
+
+#endif//PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_TABLE_H
