a5eaaa5153546d62738b9827cd34c48936586a66
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-11 19:22:55 +0200

    add ToggleButton option to Checkbox

--- src/pf_imgui/elements/Checkbox.cpp
+++ src/pf_imgui/elements/Checkbox.cpp
@@ -4,15 +4,24 @@
 
 #include "Checkbox.h"
 #include <imgui.h>
+#include <pf_imgui/details/ToggleButton.h>
 #include <toml++/toml_node_view.h>
-
 namespace pf::ui::ig {
 
 Checkbox::Checkbox(const std::string &elementName, const std::string &label, Persistent persistent, bool value)
-    : ItemElement(elementName), ValueObservable(value), Labellable(label), Savable(persistent) {}
+    : Checkbox(elementName, label, Type::Checkbox, persistent, value) {}
+
+Checkbox::Checkbox(const std::string &elementName, const std::string &label, Checkbox::Type checkboxType,
+                   Persistent persistent, bool value)
+    : ItemElement(elementName), ValueObservable(value), Labellable(label), Savable(persistent), type(checkboxType) {}
 
 void Checkbox::renderImpl() {
-  if (ImGui::Checkbox(getLabel().c_str(), getValueAddress())) { notifyValueChanged(); }
+  bool valueChanged;
+  switch (type) {
+    case Type::Checkbox: valueChanged = ImGui::Checkbox(getLabel().c_str(), getValueAddress()); break;
+    case Type::Toggle: valueChanged = ToggleButton(getLabel().c_str(), getValueAddress()); break;
+  }
+  if (valueChanged) { notifyValueChanged(); }
 }
 
 void Checkbox::unserialize_impl(const toml::table &src) { setValueAndNotifyIfChanged(src["checked"].as_boolean()); }
@@ -22,7 +31,6 @@ toml::table Checkbox::serialize_impl() { return toml::table{{{"checked", getValu
 void Checkbox::setSelected(bool value) { setValueAndNotifyIfChanged(value); }
 
 bool Checkbox::isSelected() const { return getValue(); }
-
 void Checkbox::toggle() { setSelected(!isSelected()); }
 
 }// namespace pf::ui::ig
