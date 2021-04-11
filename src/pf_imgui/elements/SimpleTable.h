f0d931763171f728c774fc2a61be4d7a0628f05f
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-11 21:09:05 +0200

    implement SimpleTable

---
+++ src/pf_imgui/elements/SimpleTable.h
@@ -0,0 +1,107 @@
+//
+// Created by petr on 4/11/21.
+//
+
+#ifndef PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_SIMPLETABLE_H
+#define PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_SIMPLETABLE_H
+
+#include <imgui.h>
+#include <pf_imgui/_export.h>
+#include <pf_imgui/interface/ItemElement.h>
+
+namespace pf::ui::ig {
+
+template<std::size_t ColumnCount>
+class PF_IMGUI_EXPORT SimpleTable : public ItemElement {
+ public:
+  using Row = std::array<std::string, ColumnCount>;
+
+  enum class Border : uint16_t {
+    None = 0b0,
+    HorizontalInner = 0b1,
+    VerticalInner = 0b10,
+    Inner = 0b11,
+    HorizontalOuter = 0b100,
+    HorizontalOuter = 0b1000,
+    Outer = 0b1100,
+    Full = 0b1111
+  };
+  struct Settings {
+    std::optional<Row> header = std::nullopt;
+    Border border = Border::None;
+    bool resizableCols = false;
+    bool reorderable = false;
+    bool sortable = false;
+    bool hideableCols = false;
+  };
+
+  explicit SimpleTable(Settings &&settings) : header(settings.header), tableFlags(createFlags(settings)) {}
+
+  void addRow(const std::ranges::range &vals) {
+    assert(std::ranges::size(vals) == ColumnCount);
+    std::ranges::copy(vals, std::back_inserter(rows));
+  }
+  void addRow(const ToStringConvertible auto &...vals) {
+    static_assert(sizeof...(vals) == ColumnCount);
+    (rows.template emplace_back(vals)...);
+  }
+  void removeRow(std::size_t index) {
+    const auto startIndex = index * ColumnCount;
+    if (startIndex >= rows.size()) { return; }
+    rows.erase(rows.begin() + startIndex, rows.begin() + startIndex + ColumnCount);
+  }
+  [[nodiscard]] std::span<std::string> getRow(std::size_it index) {
+    const auto startIndex = index * ColumnCount;
+    if (startIndex >= rows.size()) { return {}; }
+    return {rows.begin() + startIndex, rows.begin() + startIndex + ColumnCount};
+  }
+  [[nodiscard]] std::string &getCell(std::size_it row, std::size_t column) {
+    auto row = getRow(row);
+    assert(row.size() < column);
+    return row[column];
+  }
+
+ protected:
+  void renderImpl() override {
+    if (ImGui::BeginTable(getName().c_str(), ColumnCount, tableFlags)) {
+      if (header.has_value()) {
+        std::ranges::for_each(header, [](const auto &value) { ImGui::TableSetupColumn(value.c_str()); });
+        ImGui::TableHeadersRow();
+      }
+
+      std::ranges::for_each(rows | ranges::views::enumerate, [](const auto &record) {
+        const auto &[idx, value] = record;
+        if (idx % ColumnCount == 0) { ImGui::TableNextRow(); }
+        ImGui::TableNextColumn();
+        ImGui::Text(value.c_str());
+      });
+      ImGui::EndTable();
+    }
+  }
+
+ private:
+  bool is(Border lhs, Border rhs) {
+    using UnderType = std::underlying_type<Border>;
+    return static_cast<UnderType>(lhs) & static_cast<UnderType>(rhs);
+  }
+
+  static ImGuiTableFlags_ createFlags(Settings &&settings) const {
+    auto result = ImGuiTableFlags_{};
+    if (is(settings.border, Border::HorizontalInner)) { result |= ImGuiTableFlags_BordersInnerH; }
+    if (is(settings.border, Border::VerticalInner)) { result |= ImGuiTableFlags_BordersInnerV; }
+    if (is(settings.border, Border::HorizontalOuter)) { result |= ImGuiTableFlags_BordersOuterH; }
+    if (is(settings.border, Border::VerticalOuter)) { result |= ImGuiTableFlags_BordersOuterV; }
+    if (settings.resizableCols) { result |= ImGuiTableFlags_Resizable; }
+    if (settings.reorderable) { result |= ImGuiTableFlags_Reorderable; }
+    if (settings.sortable) { result |= ImGuiTableFlags_Sortable; }
+    if (settings.hideableCols) { result |= ImGuiTableFlags_Hideable; }
+    return result;
+  }
+  std::optional<Row> header;
+  std::vector<std::string> rows;
+  ImGuiTableFlags_ tableFlags;
+};
+
+}// namespace pf::ui::ig
+
+#endif//PF_IMGUI_SRC_PF_IMGUI_ELEMENTS_SIMPLETABLE_H
