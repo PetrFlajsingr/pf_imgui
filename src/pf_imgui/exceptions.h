bdb42a64ebadd07c926874683ccfab99b17733cd
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-13 20:40:11 +0200

    add doc for exceptions

--- src/pf_imgui/exceptions.h
+++ src/pf_imgui/exceptions.h
@@ -9,9 +9,12 @@
 
 namespace pf::ui::ig {
 
+/**
+ * Exception thrown when a duplicate ID is added in the UI tree.
+ */
 class DuplicateIdException : public StackTraceException {
  public:
-  DuplicateIdException(const std::string_view &fmt, auto &&...args)
+  explicit DuplicateIdException(const std::string_view &fmt, auto &&...args)
       : StackTraceException(StackTraceException::fmt(fmt, args...)) {}
 };
 }// namespace pf::ui::ig
