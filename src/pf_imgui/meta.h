6e1b3f79b6ef746e5ced315119ac7f951d74a37f
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-13 21:02:56 +0200

    add doc for meta

--- src/pf_imgui/meta.h
+++ src/pf_imgui/meta.h
@@ -10,9 +10,15 @@
 
 namespace pf::ui::ig {
 
+/**
+ * Check if the type is a specialization of ValueObservable.
+ * @tparam T type to be checked
+ */
 template<typename T>
 constexpr auto IsValueObservable = derived_specialization_of<T, ValueObservable>;
-
+/**
+ * Placeholder type to avoid compilation errors.
+ */
 struct CommonPlaceholder {
   using CreateType = int;
 };
