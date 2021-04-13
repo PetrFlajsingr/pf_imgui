d3e34d20c339516b21ab1fb6ee8470a71cde3d12
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-13 15:57:52 +0200

    add doc for Clickable

--- src/pf_imgui/interface/Clickable.h
+++ src/pf_imgui/interface/Clickable.h
@@ -14,13 +14,26 @@
 
 namespace pf::ui::ig {
 
+/**
+ * @brief Interface for clickable elements
+ *
+ * Provides the ability to add multiple observers and also remove them via @see Subscription
+ */
 class PF_IMGUI_EXPORT Clickable {
  public:
+  /**
+   * Add a parameterless listener, which is called every time the item is clicked.
+   * @param fnc listener for click event
+   * @return instance of @see Subscription, which allows to unsubscribe the listener
+   */
   Subscription addClickListener(std::invocable auto fnc) { return observableImpl.template addListener(fnc); }
 
   virtual ~Clickable() = default;
 
  protected:
+  /**
+   * Call all listeners.
+   */
   void notifyOnClick();
 
  private:
