85723e9ab67a16c5a6f9989c7c3252a7c60f2de0
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-13 18:38:04 +0200

    add doc for Savable

--- src/pf_imgui/interface/Savable.h
+++ src/pf_imgui/interface/Savable.h
@@ -12,20 +12,45 @@
 
 namespace pf::ui::ig {
 
+/**
+ * @brief Interface for savable elements
+ *
+ * Provides a functionality to save subclasses' state and load it back up.
+ */
 class PF_IMGUI_EXPORT Savable {
  public:
+  /**
+   * Construct Savable with enabled/disabled saving capability
+   * @param persistent
+   */
   explicit Savable(Persistent persistent);
 
   Savable(Savable &&other) noexcept;
   Savable &operator=(Savable &&other) noexcept;
 
+  /**
+   * Load data from toml.
+   * @param src source toml data
+   */
   void unserialize(const toml::table &src);
+  /**
+   * Save data to toml.
+   * @return toml::table if saving is enabled, nullopt otherwise
+   */
   std::optional<toml::table> serialize();
 
   virtual ~Savable() = default;
 
  protected:
+  /**
+   * Method for subclasses' implementation of deserialization.
+   * @param src source toml data
+   */
   virtual void unserialize_impl(const toml::table &src) = 0;
+  /**
+   * Method for subclasses' implementation of serialization.
+   * @return serialized toml data
+   */
   virtual toml::table serialize_impl() = 0;
 
  private:
