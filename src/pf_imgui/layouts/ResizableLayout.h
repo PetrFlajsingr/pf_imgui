d1a2154117d9e9f15dae9afb14fe8591a20453bb
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-14 17:48:45 +0200

    add docs for ResizableLayout

--- src/pf_imgui/layouts/ResizableLayout.h
+++ src/pf_imgui/layouts/ResizableLayout.h
@@ -13,12 +13,30 @@
 
 namespace pf::ui::ig {
 
+/**
+ * @brief An interface for Layouts, which support changing their size.
+ *
+ * Interface for common capabilities required by layouts.
+ */
 class PF_IMGUI_EXPORT ResizableLayout : public Layout, public Resizable {
  public:
+  /**
+   * Construct ResizableLayout.
+   * @param elementName ID of the layout
+   * @param size size of the layout
+   * @param allowCollapse enable collapse button
+   * @param showBorder draw layouts border
+   * @param persistent allow state saving
+   */
   ResizableLayout(const std::string &elementName, const ImVec2 &size, AllowCollapse allowCollapse,
                   ShowBorder showBorder, Persistent persistent);
 
  protected:
+  /**
+   * Check whether the layout is collapsed or not and calculate its size based on that.
+   * @attention Should be used in descendants to provide common functionality.
+   * @return size of the layout for rendering purposes
+   */
   [[nodiscard]] ImVec2 getSizeIfCollapsed() const;
 };
 
