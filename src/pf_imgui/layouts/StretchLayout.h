6149bc0ad03369209f1e4aecefdb2f263099c277
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-14 18:22:45 +0200

    add docs for StretchLayout

--- src/pf_imgui/layouts/StretchLayout.h
+++ src/pf_imgui/layouts/StretchLayout.h
@@ -10,16 +10,69 @@
 #include <pf_imgui/enums.h>
 
 namespace pf::ui::ig {
+/**
+ * @brief A layout which stretches to the available area in desired direction.
+ *
+ *
+ */
 class StretchLayout : public ResizableLayout {
  public:
+  /**
+   * Construct StretchLayout.
+   * @param elementName ID of the layout
+   * @param size size of the layout
+   * @param stretch dimension to which the layout stretches
+   * @param allowCollapse enable collapse button
+   * @param showBorder draw border around the layout
+   * @param persistent enable state saving
+   */
   StretchLayout(const std::string &elementName, const ImVec2 &size, Stretch stretch,
                 AllowCollapse allowCollapse = AllowCollapse::No, ShowBorder showBorder = ShowBorder::No,
                 Persistent persistent = Persistent::No);
+  /**
+   * Construct StretchLayout.
+   * @param elementName ID of the layout
+   * @param size size of the layout
+   * @param stretch dimension to which the layout stretches
+   * @param showBorder draw border around the layout
+   * @param persistent enable state saving
+   */
   StretchLayout(const std::string &elementName, const ImVec2 &size, Stretch stretch, ShowBorder showBorder,
                 Persistent persistent = Persistent::No);
+  /**
+   * Construct StretchLayout.
+   * @param elementName ID of the layout
+   * @param size size of the layout
+   * @param stretch dimension to which the layout stretches
+   * @param allowCollapse enable collapse button
+   * @param persistent enable state saving
+   */
   StretchLayout(const std::string &elementName, const ImVec2 &size, Stretch stretch, AllowCollapse allowCollapse,
                 Persistent persistent = Persistent::No);
 
+  /**
+  * Create a child and append it to the end of children.
+  *
+  * @tparam T type of created Element
+  * @tparam Args arguments to pass to the Ts constructor after its name
+  * @param name ID of the newly created element
+  * @param position position of the newly created element
+  * @param args arguments to pass to the Ts constructor after its nam
+  * @return reference to the newly created Element
+  *
+  * @throws DuplicateIdException when an ID is already present in the container
+  */
+
+  /**
+   * Create a child inside the layout. If the layout already contains one it gets overwritten.
+   *
+   * @tparam T type of created Element
+   * @tparam Args arguments to pass to the Ts constructor after its name
+   * @param name ID of the newly created element
+   * @param args arguments to pass to the Ts constructor after its nam
+   * @return reference to the newly created Element
+   *
+   */
   template<typename T, typename... Args>
   requires std::derived_from<T, Element> && std::derived_from<T, Resizable> && std::constructible_from<T, std::string,
                                                                                                        Args...>
@@ -29,12 +82,38 @@ class StretchLayout : public ResizableLayout {
     return *child;
   }
 
+  /**
+   * Set child inside the layout. If the layout already contains one it gets overwritten.
+   * @param newChild
+   */
+  template<std::derived_from<Element> T>
+  void setChild(std::unique_ptr<T> &&newChild) {
+    child = std::move(newChild);
+  }
+
   std::vector<Renderable *> getRenderables() override;
 
- public:
+  /**
+   * Get child of the layout.
+   * @return reference to the child
+   * @throws StackTraceException when no child is present inside the layout
+   */
   [[nodiscard]] Element &getChild();
+  /**
+   * Check whether the layout contains a child.
+   * @return true if the layout contains a child, false otherwise
+   */
+  [[nodiscard]] bool hasChild() const;
 
+  /**
+   * Get current stretch setting.
+   * @return current stretch setting
+   */
   [[nodiscard]] Stretch getStretch() const;
+  /**
+   * Set new stretch setting.
+   * @param newStretch new stretch setting
+   */
   void setStretch(Stretch newStretch);
 
  protected:
