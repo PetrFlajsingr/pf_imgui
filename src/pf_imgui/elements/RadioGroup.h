c57dba1c0065aa45a0629791e117cbf1066ee32c
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-15 18:27:05 +0200

    add docs RadioGroup

--- src/pf_imgui/elements/RadioGroup.h
+++ src/pf_imgui/elements/RadioGroup.h
@@ -14,15 +14,38 @@
 
 namespace pf::ui::ig {
 
+/**
+ * @brief Group of RadioButton.
+ *
+ * Only one button in a group can be selected at one time.
+ *
+ * Label of the selected button is observable.
+ *
+ * @todo: existing button to group addition support
+ */
 class PF_IMGUI_EXPORT RadioGroup : public Element,
                                    public Labellable,
                                    public ValueObservable<std::string_view>,
                                    public Savable {
  public:
+  /**
+   * Construct RadioGroup.
+   * @param elementName ID of the element
+   * @param label text rendered above the group
+   * @param buttons buttons in the group
+   * @param selectedButtonIndex index of selected button
+   * @param persistent enable state saving to disk
+   */
   RadioGroup(const std::string &elementName, const std::string &label, std::vector<RadioButton> buttons = {},
              const std::optional<std::size_t> &selectedButtonIndex = std::nullopt,
              Persistent persistent = Persistent::No);
 
+  /**
+   * Create a new button and add it to the group.
+   * @param elementName ID of the button
+   * @param caption text rendered next to the button
+   * @param value true for selected, false otherwise
+   */
   void addButton(const std::string &elementName, const std::string &caption, bool value = false);
 
  protected:
