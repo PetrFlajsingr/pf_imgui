64106178a1d9d3b1c25eb1111ea6b39d734f701e
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-15 18:36:44 +0200

    add docs MenuBars

--- src/pf_imgui/elements/MenuBars.h
+++ src/pf_imgui/elements/MenuBars.h
@@ -19,12 +19,30 @@ namespace pf::ui::ig {
 
 class SubMenu;
 class MenuItem;
-
+/**
+ * @brief An item which can contain other menus.
+ */
 class PF_IMGUI_EXPORT MenuContainer {
  public:
+  /**
+   * Create an instance of SubMenu and add it to the end if children.
+   * @param name ID of the SubMenu
+   * @param caption text of the SubMenu
+   * @return reference to the created SubMenu
+   */
   SubMenu &addSubmenu(const std::string &name, const std::string &caption);
+  /**
+   * Create an instance of MenuItem and add it to the end of children/
+   * @param name ID of the MenuItem
+   * @param caption text of the MenuItem
+   * @return reference to the created MenuItem
+   */
   MenuItem &addItem(const std::string &name, const std::string &caption);
 
+  /**
+   * Remove item by ID.
+   * @param name ID of the item to be removed
+   */
   void removeItem(const std::string &name);
 
  protected:
@@ -33,33 +51,63 @@ class PF_IMGUI_EXPORT MenuContainer {
  private:
   std::vector<std::unique_ptr<Element>> items;
 };
-
+/**
+ * @brief An item in SubMenu, which can be clicked. It is basically a popup menu item.
+ */
 class PF_IMGUI_EXPORT MenuItem : public Element, public Labellable, public Clickable {
  public:
+  /**
+   * Construct MenuItem.
+   * @param elementName ID of the element
+   * @param label text rendered on the button
+   */
   MenuItem(const std::string &elementName, const std::string &label);
 
  protected:
   void renderImpl() override;
 };
-
+/**
+ * @brief Basically a button for a popup menu, which contains other elements as its items.
+ */
 class PF_IMGUI_EXPORT SubMenu : public Element, public Labellable, public MenuContainer {
  public:
+  /**
+   * Construct SubMenu.
+   * @param elementName ID of the element
+   * @param label text rendered on the button
+   */
   SubMenu(const std::string &elementName, const std::string &label);
 
  protected:
   void renderImpl() override;
 };
-
+/**
+ * @brief Menu bar belonging to Window.
+ *
+ * There can be one menu bar for each Window instance. It is created via Window::getMenuBar().
+ */
 class PF_IMGUI_EXPORT WindowMenuBar : public Element, public MenuContainer {
  public:
+  /**
+   * Construct WindowMenuBar.
+   * @param elementName ID of the element
+   */
   explicit WindowMenuBar(const std::string &elementName);
 
  protected:
   void renderImpl() override;
 };
-
+/**
+ * @brief Menu bar belonging to the entire application window.
+ *
+ * Only one of these can exist at a time. It is created via ImGuiInterface::getMenuBar().
+ */
 class PF_IMGUI_EXPORT AppMenuBar : public Element, public MenuContainer {
  public:
+  /**
+   * Construct AppMenuBar.
+   * @param elementName ID of the element
+   */
   explicit AppMenuBar(const std::string &elementName);
 
  protected:
