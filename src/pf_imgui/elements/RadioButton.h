6ba25d59ccae21860bee58d8cec10493bd332bce
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-15 18:29:25 +0200

    add docs RadioButton

--- src/pf_imgui/elements/RadioButton.h
+++ src/pf_imgui/elements/RadioButton.h
@@ -13,11 +13,24 @@
 
 namespace pf::ui::ig {
 
+/**
+ * @brief A button similar in function to CheckBox, but it can belong to RadioGroup.
+ */
 class PF_IMGUI_EXPORT RadioButton : public ItemElement, public Labellable, public ValueObservable<bool> {
  public:
   friend class RadioGroup;
+  /**
+   * Construct RadioButton.
+   * @param elementName ID of the button
+   * @param label text rendered next to the button
+   * @param value starting value of the button
+   */
   RadioButton(const std::string &elementName, const std::string &label, bool value = false);
 
+  /**
+   * Check if the button is selected.
+   * @return true when the button is selected, false otherwise
+   */
   [[nodiscard]] bool isSelected() const;
 
  protected:
