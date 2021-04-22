17ab48458cdc623cb5dd3da72bf03600fbfd5ab3
petr <xflajs00@stud.fit.vutbr.cz>
2021-04-22 18:44:30 +0200

    add Tooltip to DragSource

---
+++ src/pf_imgui/interface/DragNDrop.cpp
@@ -0,0 +1,75 @@
+//
+// Created by petr on 4/22/21.
+//
+
+#include "DragNDrop.h"
+#include <pf_imgui/elements/Text.h>
+
+namespace pf::ui::ig {
+
+details::DragSourceBase::DragSourceBase(bool dragAllowed) : dragAllowed(dragAllowed) {}
+
+bool details::DragSourceBase::isDragAllowed() const { return dragAllowed; }
+
+void details::DragSourceBase::setDragAllowed(bool allowed) { dragAllowed = allowed; }
+
+bool details::DragSourceBase::isDragged() const { return dragged; }
+
+bool details::DragSourceBase::drag_impl(const std::string &typeName, void *sourceData, std::size_t dataSize) {
+  auto flags = ImGuiDragDropFlags_SourceAllowNullID;
+  if (dragAllowed && ImGui::BeginDragDropSource(flags)) {
+    dragged = true;
+    ImGui::SetDragDropPayload(typeName.c_str(), sourceData, dataSize);
+    if (tooltip != nullptr) { tooltip->render(); }
+    ImGui::EndDragDropSource();
+    auto payload = ImGui::GetDragDropPayload();
+    return payload != nullptr && payload->IsDelivery();// check if this detection works
+  } else {
+    dragged = false;
+  }
+  return false;
+}
+bool details::DragSourceBase::hasDragTooltip() const { return tooltip != nullptr; }
+Tooltip &details::DragSourceBase::getDragTooltip() {
+  if (tooltip == nullptr) { throw StackTraceException("Drag tooltip doesn't exist."); }
+  return *tooltip;
+}
+Tooltip &details::DragSourceBase::createDragTooltip() {
+  tooltip = std::make_unique<Tooltip>(uniqueId());
+  tooltipTextFmt = std::nullopt;
+  return *tooltip;
+}
+void details::DragSourceBase::removeDragTooltip() {
+  tooltip = nullptr;
+  tooltipTextFmt = std::nullopt;
+}
+bool details::DragSourceBase::drag_impl_fmt(const std::string &typeName, void *sourceData, std::size_t dataSize,
+                                            std::string value) {
+  if (tooltipTextFmt.has_value()) { tooltipTextFmt->second->setText(fmt::format(tooltipTextFmt->first, value)); }
+  return drag_impl(typeName, sourceData, dataSize);
+}
+void details::DragSourceBase::createSimpleTooltip(std::string fmt, bool isValueFmt) {
+  auto &tooltipText = createDragTooltip().createChild<Text>(uniqueId(), std::string(fmt));
+  if (isValueFmt) {
+    tooltipTextFmt = std::make_pair(fmt, &tooltipText);
+  } else {
+    tooltipTextFmt = std::nullopt;
+  }
+}
+
+details::DropTargetBase::DropTargetBase(bool dropAllowed) : dropAllowed(dropAllowed) {}
+
+bool details::DropTargetBase::isDropAllowed() const { return dropAllowed; }
+
+void details::DropTargetBase::setDropAllowed(bool allowed) { dropAllowed = allowed; }
+
+std::optional<void *> details::DropTargetBase::dropAccept_impl(const std::string &typeName) const {
+  if (dropAllowed && ImGui::BeginDragDropTarget()) {
+    auto payload = ImGui::AcceptDragDropPayload(typeName.c_str());// todo type conversions
+    void *result = payload == nullptr ? nullptr : payload->Data;
+    ImGui::EndDragDropTarget();
+    if (result != nullptr) { return result; }
+  }
+  return std::nullopt;
+}
+}// namespace pf::ui::ig
--- src/pf_imgui/interface/DragNDrop.h
+++ src/pf_imgui/interface/DragNDrop.h
@@ -7,9 +7,13 @@
 
 #include <algorithm>
 #include <concepts>
+#include <fmt/ostream.h>
 #include <imgui.h>
 #include <optional>
+#include <pf_common/concepts/StringConvertible.h>
 #include <pf_imgui/elements/Tooltip.h>
+#include <pf_imgui/fwd.h>
+#include <pf_imgui/unique_id.h>
 #include <ranges>
 #include <static_type_info.h>
 #include <vector>
@@ -18,82 +22,190 @@ namespace pf::ui::ig {
 
 // TODO: type conversions, user customisation
 // TODO: type IDs
-// TODO: some tooltip formatting for transported value
 
 namespace details {
+/**
+ * @brief Base class for templated DragSource<T>
+ *
+ * Enables content dragging from this element. Provides an ability to add tooltip to dragged data.
+ */
 class DragSourceBase {
  public:
-  inline explicit DragSourceBase(bool dragAllowed) : dragAllowed(dragAllowed) {}
-  [[nodiscard]] inline bool isDragAllowed() const { return dragAllowed; }
-  inline void setDragAllowed(bool allowed) { dragAllowed = allowed; }
-  [[nodiscard]] inline bool isDragged() const { return dragged; }
+  /**
+   * Construct DragSourceBase.
+   * @param dragAllowed enable/disable drag
+   */
+  explicit DragSourceBase(bool dragAllowed);
+  /**
+   * Check if dragging is allowed.
+   * @return true if the drag is allowed
+   */
+  [[nodiscard]] bool isDragAllowed() const;
+  /**
+   * Enable/disable dragging.
+   * @param allowed
+   */
+  void setDragAllowed(bool allowed);
+  /**
+   * Check if content is currently dragged.
+   * @return true if content is dragged
+   */
+  [[nodiscard]] bool isDragged() const;
+
+  /**
+   * Check if dragging has an active tooltip.
+   * @return true if the tooltip is active, false otherwise
+   */
+  [[nodiscard]] bool hasDragTooltip() const;
+  /**
+   * Get the drag tooltip if it exists.
+   * @return tooltip of drag
+   *
+   * @throws StacktraceException if the tooltip doesn't exist
+   */
+  [[nodiscard]] Tooltip &getDragTooltip();
+  /**
+   * Create an instance of tooltip for drag to be filled with elements by the user.
+   * @return reference to the newly created tooltip
+   */
+  [[nodiscard]] Tooltip &createDragTooltip();
+  /**
+   * Removes drag's tooltip if it is assigned. If the tooltip doesn't exist, nothing happens.
+   */
+  void removeDragTooltip();
 
   virtual ~DragSourceBase() = default;
-  // TODO: tooltip
 
  protected:
-  inline bool drag_impl(const std::string &typeName, void *sourceData, std::size_t dataSize) {
-    auto flags = ImGuiDragDropFlags_SourceAllowNullID;
-    if (dragAllowed && ImGui::BeginDragDropSource(flags)) {
-      dragged = true;
-      ImGui::SetDragDropPayload(typeName.c_str(), sourceData, dataSize);
-      if (tooltip.has_value()) { tooltip->render(); }
-      ImGui::EndDragDropSource();
-      auto payload = ImGui::GetDragDropPayload();
-      return payload != nullptr && payload->IsDelivery();// check if this detection works
-    } else {
-      dragged = false;
-    }
-    return false;
-  }
-  bool dragged = false;
+  /**
+   * Run drag source code if allowed. Renders the tooltip as well.
+   * @param typeName type of transferred data
+   * @param sourceData transferred data
+   * @param dataSize size of transferred data
+   * @return true if content is being dragged, false otherwise
+   */
+  bool drag_impl(const std::string &typeName, void *sourceData, std::size_t dataSize);
+  /**
+   * Run drag source code if allowed. Renders the tooltip as well.
+   * @param typeName type of transferred data
+   * @param sourceData transferred data
+   * @param dataSize size of transferred data
+   * @param value string to be inserted into simple tooltip
+   * @return true if content is being dragged, false otherwise
+   */
+  bool drag_impl_fmt(const std::string &typeName, void *sourceData, std::size_t dataSize, std::string value);
   bool dragAllowed;
-  std::optional<Tooltip> tooltip;
+
+  /**
+   * Create a tooltip possibly with format capability.
+   * @param fmt text to show/format string in case of isValueFmt
+   * @param isValueFmt true if fmt contains {} for formatting
+   */
+  void createSimpleTooltip(std::string fmt, bool isValueFmt);
+ private:
+  bool dragged = false;
+  std::unique_ptr<Tooltip> tooltip = nullptr;
+  std::optional<std::pair<std::string, Text *>> tooltipTextFmt;
 };
 
+/**
+ * @brief Base class for templated DropTarget<T>
+ *
+ * Enables content drop from DragSource.
+ */
 class DropTargetBase {
  public:
-  inline explicit DropTargetBase(bool dropAllowed) : dropAllowed(dropAllowed) {}
-  [[nodiscard]] inline bool isDropAllowed() const { return dropAllowed; }
-  inline void setDropAllowed(bool allowed) { dropAllowed = allowed; }
+  /**
+   * Construct DropTargetBase.
+   * @param dropAllowed enable/disable as drop target
+   */
+  explicit DropTargetBase(bool dropAllowed);
+  /**
+   * Check whether this object functions as a drop target.
+   * @return true if this object functions as a drop target
+   */
+  [[nodiscard]] bool isDropAllowed() const;
+  /**
+   * Enable/disable this object as drop target.
+   * @param allowed
+   */
+  void setDropAllowed(bool allowed);
 
   virtual ~DropTargetBase() = default;
 
  protected:
-  [[nodiscard]] inline std::optional<void *> dropAccept_impl(const std::string &typeName) const {
-    if (dropAllowed && ImGui::BeginDragDropTarget()) {
-      auto payload = ImGui::AcceptDragDropPayload(typeName.c_str());// todo type conversions
-      void *result = payload == nullptr ? nullptr : payload->Data;
-      ImGui::EndDragDropTarget();
-      if (result != nullptr) { return result; }
-    }
-    return std::nullopt;
-  }
+  /**
+   * Implementation of drop acceptance. Controlled by dropAllowed.
+   * @param typeName
+   * @return
+   */
+  [[nodiscard]] std::optional<void *> dropAccept_impl(const std::string &typeName) const;
 
   bool dropAllowed;
 };
 }// namespace details
 class DragNDropGroup;
 
+/**
+ * @brief Class providing the ability for derived to be a source of data for drag and drop operation.
+ * @tparam T type of transferred data
+ *
+ * You have to use drag(...) function in order to activate drag source.
+ */
 template<typename T>
 class DragSource : public details::DragSourceBase {
  public:
+  /**
+   * Construct DragSource.
+   * @param dragAllowed enable/disable drag
+   */
   explicit DragSource(bool dragAllowed) : DragSourceBase(dragAllowed) {}
 
+  /**
+   * Set simple tooltip for dragging. If the text contains '{}' and T is ToStringConvertible than the value transferred is formatted to the string.
+   * @param text text on tooltip
+   */
+  void setDragTooltip(std::string_view text) {
+    createSimpleTooltip(std::string(text), text.find("{}") != std::string_view::npos);
+  }
+
  protected:
+  /**
+   * Call this method to start potential dragging.
+   * @param sourceData data to be transferred
+   * @return true if the value has been transferred
+   */
   bool drag(const T *sourceData) {
     const auto ptrPtr = &sourceData;
-    return drag_impl(std::string(static_type_info::getTypeName<T>().substr(0, 32)),
-                     reinterpret_cast<void *>(const_cast<T **>(ptrPtr)), sizeof(const T *));
+    if constexpr (ToStringConvertible<T>) {
+      return drag_impl_fmt(std::string(static_type_info::getTypeName<T>().substr(0, 32)),
+                           reinterpret_cast<void *>(const_cast<T **>(ptrPtr)), sizeof(const T *),
+                           toString(*sourceData));
+    } else {
+      return drag_impl(std::string(static_type_info::getTypeName<T>().substr(0, 32)),
+                       reinterpret_cast<void *>(const_cast<T **>(ptrPtr)), sizeof(const T *));
+    }
   }
 };
 
+/**
+ * @brief Target for drag and drop operation.
+ * @tparam T type of data which this object accepts
+ */
 template<typename T>
 class DropTarget : public details::DropTargetBase {
  public:
+  /**
+   * Construct DropTarget.
+   * @param dropAllowed enable/disable drop target
+   */
   explicit DropTarget(bool dropAllowed) : DropTargetBase(dropAllowed) {}
 
  protected:
+  /**
+   * Call this method in order to accept potential transferred data.
+   * @return std::nullopt if no value was accepted, the value otherwise
+   */
   std::optional<T> dropAccept() {
     const auto dropResult = dropAccept_impl(std::string(static_type_info::getTypeName<T>().substr(0, 32)));
     if (dropResult.has_value()) { return **reinterpret_cast<const T **>(*dropResult); }
