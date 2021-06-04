ee4442d6007997c3daf66e892bf274072dbed2a7
petr <xflajs00@stud.fit.vutbr.cz>
2021-06-04 21:16:33 +0200

    make CustomCombobox and CustomListbox inherit from ItemBox

--- src/pf_imgui/elements/CustomCombobox.h
+++ src/pf_imgui/elements/CustomCombobox.h
@@ -8,18 +8,9 @@
 #include <imgui.h>
 #include <pf_common/enums.h>
 #include <pf_imgui/_export.h>
-#include <pf_imgui/interface/ItemElement.h>
-#include <pf_imgui/interface/Labellable.h>
+#include <pf_imgui/elements/CustomItemBox.h>
 
 namespace pf::ui::ig {
-/**
- * Factory of Combobox's rows.
- * @tparam F factory
- * @tparam T input/storage type
- * @tparam R resulting renderable for row
- */
-template<typename F, typename T, typename R>
-concept CustomComboboxRowFactory = std::is_invocable_r_v<std::unique_ptr<R>, F, T>;
 
 enum class ComboBoxCount { Items4 = 1 << 1, Items8 = 1 << 2, Items20 = 1 << 3, ItemsAll = 1 << 4 };
 
@@ -31,7 +22,7 @@ enum class ComboBoxCount { Items4 = 1 << 1, Items8 = 1 << 2, Items20 = 1 << 3, I
  * @tparam R type stored in each row
  */
 template<typename T, std::derived_from<Renderable> R>
-class PF_IMGUI_EXPORT CustomCombobox : public ItemElement, public Labellable {
+class PF_IMGUI_EXPORT CustomCombobox : public CustomItemBox<T, R> {
  public:
   /**
    * Construct CustomCombobox.
@@ -41,97 +32,11 @@ class PF_IMGUI_EXPORT CustomCombobox : public ItemElement, public Labellable {
    * @param prevValue preview value
    * @param showItemCount amount of items shown when open
    */
-  CustomCombobox(const std::string &elementName, const std::string &label,
-                 CustomComboboxRowFactory<T, R> auto &&rowFactory, std::string prevValue = "",
-                 ComboBoxCount showItemCount = ComboBoxCount::Items8)
-      : ItemElement(elementName), Labellable(label), factory(std::forward<decltype(rowFactory)>(rowFactory)),
+  CustomCombobox(const std::string &elementName, const std::string &label, CustomItemBox<T, R> auto &&rowFactory,
+                 std::string prevValue = "", ComboBoxCount showItemCount = ComboBoxCount::Items8)
+      : CustomItemBox<T, R>(elementName, label, std::forward<decltype(rowFactory)>(rowFactory)),
         flags(static_cast<ImGuiComboFlags_>(showItemCount)), previewValue(std::move(prevValue)) {
     if (previewValue.empty()) { flags |= ImGuiComboFlags_::ImGuiComboFlags_NoPreview; }
-  }
-
-  /**
-   * Add item to the end of the list.
-   * @param item item to be added
-   * @return reference to the Renderable representing the newly created row
-   */
-  R &addItem(const T &item) {
-    auto &result = *items.template emplace_back(item, factory(item)).second.get();
-    refilterItems();
-    return result;
-  }
-
-  /**
-   * Add multiple item to the end of the list.
-   * @param newItems items to be added
-   * @return references to the newly created rows
-   */
-  std::vector<std::reference_wrapper<R>> addItems(std::ranges::range auto &&newItems) requires(
-      std::convertible_to<std::ranges::range_value_t<decltype(newItems)>, T>) {
-    auto result = std::vector<std::reference_wrapper<R>>{};
-    std::ranges::for_each(newItems, [this, &result](const auto &newItem) {
-      result.template emplace_back(*items.template emplace_back(newItem, factory(newItem)).second.get());
-    });
-    refilterItems();
-    return result;
-  }
-
-  /**
-   * Clear the listbox and set new items.
-   * @param newItems items to set
-   * @return references to the newly created rows
-   */
-  std::vector<std::reference_wrapper<R>> setItems(std::ranges::range auto &&newItems) requires(
-      std::convertible_to<std::ranges::range_value_t<decltype(newItems)>, T>) {
-    items.clear();
-    return addItems(std::forward<decltype(newItems)>(newItems));
-  }
-
-  /**
-   * Remove an item.
-   * @param itemToRemove item to remove
-   */
-  void removeItem(const T &itemToRemove) requires(std::equality_comparable<T>) {
-    std::erase_if(items, [this, &itemToRemove](const auto &item) { return item.first == itemToRemove; });
-    refilterItems();
-  }
-  /**
-   * Remove all items for which predicate returns true.
-   * @param predicate predicate for item removal
-   */
-  void removeItemIf(std::predicate<const T &> auto &&predicate) {
-    std::erase_if(items,
-                  [p = std::forward<decltype(predicate)>(predicate)](const auto &item) { return p(item.first); });
-    refilterItems();
-  }
-
-  /**
-   * Set filter by which shown items are filtered.
-   * @param filterFnc
-   */
-  void setFilter(std::predicate<const T &> auto filterFnc) {
-    filter = filterFnc;
-    refilterItems();
-  }
-  /**
-   * Remove filter.
-   */
-  void clearFilter() {
-    filter = [](auto) { return true; };
-    refilterItems();
-  }
-  /**
-   * Get reference to all items.
-   * @return references to all items
-   */
-  [[nodiscard]] auto getItems() {
-    return items | std::views::transform([](auto &item) -> T & { return item.first; });
-  }
-  /**
-   * Get reference to all items.
-   * @return references to all items
-   */
-  [[nodiscard]] auto getItems() const {
-    return items | std::views::transform([](auto &item) -> const T & { return item.first; });
   }
 
   void setPreviewValue(std::string value) {
@@ -144,7 +49,7 @@ class PF_IMGUI_EXPORT CustomCombobox : public ItemElement, public Labellable {
    * Get count of items shown when the element is unrolled.
    * @return count of items
    */
-  [[nodiscard]] ComboBoxCount getShownItemCount() const {// FIXME
+  [[nodiscard]] ComboBoxCount getShownItemCount() const {
     return static_cast<ComboBoxCount>(*(flags & ImGuiComboFlags_::ImGuiComboFlags_HeightMask_));
   }
   /**
@@ -162,14 +67,6 @@ class PF_IMGUI_EXPORT CustomCombobox : public ItemElement, public Labellable {
   void close() { shouldClose = true; }
 
  protected:
-  /**
-   * Recreate filteredItems.
-   */
-  virtual void refilterItems() {
-    filteredItems = items | std::views::filter([this](auto &item) { return filter(item.first); })
-        | std::views::transform([](auto &item) { return &item; }) | ranges::to_vector;
-  }
-
   void renderImpl() override {
     const char *previewPtr = previewValue.c_str();
     if (ImGui::BeginCombo(getLabel().c_str(), previewPtr, *flags)) {
@@ -193,12 +90,6 @@ class PF_IMGUI_EXPORT CustomCombobox : public ItemElement, public Labellable {
       shouldClose = false;
     }
   }
-
-  using Item = std::pair<T, std::unique_ptr<R>>;
-  std::vector<Item> items;
-  std::vector<Item *> filteredItems;
-  std::function<std::unique_ptr<R>(const T &)> factory;
-  std::function<bool(const T &)> filter = [](const auto &) { return true; };
 
   Flags<ImGuiComboFlags_> flags{};
 
--- src/pf_imgui/elements/CustomListbox.h
+++ src/pf_imgui/elements/CustomListbox.h
@@ -10,21 +10,11 @@
 
 #include <algorithm>
 #include <pf_imgui/_export.h>
-#include <pf_imgui/interface/ItemElement.h>
-#include <pf_imgui/interface/Labellable.h>
+#include <pf_imgui/elements/CustomItemBox.h>
 #include <pf_imgui/interface/Resizable.h>
 #include <vector>
 
 namespace pf::ui::ig {
-
-/**
- * Factory of Listbox's rows.
- * @tparam F factory
- * @tparam T input/storage type
- * @tparam R resulting renderable for row
- */
-template<typename F, typename T, typename R>
-concept CustomListboxRowFactory = std::is_invocable_r_v<std::unique_ptr<R>, F, T>;
 
 /**
  * @brief Listbox where rows can be whatever is provided by the user.
@@ -34,7 +24,7 @@ concept CustomListboxRowFactory = std::is_invocable_r_v<std::unique_ptr<R>, F, T
  * @tparam R type stored in each row
  */
 template<typename T, std::derived_from<Renderable> R>
-class PF_IMGUI_EXPORT CustomListbox : public ItemElement, public Labellable, public Resizable {
+class PF_IMGUI_EXPORT CustomListbox : public CustomItemBox<T, R>, public Resizable {
  public:
   /**
    * Create CustomListbox
@@ -45,111 +35,15 @@ class PF_IMGUI_EXPORT CustomListbox : public ItemElement, public Labellable, pub
    */
   CustomListbox(const std::string &elementName, const std::string &label,
                 CustomListboxRowFactory<T, R> auto &&rowFactory, Size s = Size::Auto())
-      : ItemElement(elementName), Labellable(label), Resizable(s),
-        factory(std::forward<decltype(rowFactory)>(rowFactory)) {}
-
-  /**
-   * Add item to the end of the list.
-   * @param item item to be added
-   * @return reference to the Renderable representing the newly created row
-   */
-  R &addItem(const T &item) {
-    auto &result = *items.template emplace_back(item, factory(item)).second.get();
-    refilterItems();
-    return result;
-  }
-
-  /**
-   * Add multiple item to the end of the list.
-   * @param newItems items to be added
-   * @return references to the newly created rows
-   */
-  std::vector<std::reference_wrapper<R>> addItems(std::ranges::range auto &&newItems) requires(
-      std::convertible_to<std::ranges::range_value_t<decltype(newItems)>, T>) {
-    auto result = std::vector<std::reference_wrapper<R>>{};
-    std::ranges::for_each(newItems, [this, &result](const auto &newItem) {
-      result.template emplace_back(*items.template emplace_back(newItem, factory(newItem)).second.get());
-    });
-    refilterItems();
-    return result;
-  }
-
-  /**
-   * Clear the listbox and set new items.
-   * @param newItems items to set
-   * @return references to the newly created rows
-   */
-  std::vector<std::reference_wrapper<R>> setItems(std::ranges::range auto &&newItems) requires(
-      std::convertible_to<std::ranges::range_value_t<decltype(newItems)>, T>) {
-    items.clear();
-    return addItems(std::forward<decltype(newItems)>(newItems));
-  }
-
-  /**
-   * Remove an item.
-   * @param itemToRemove item to remove
-   */
-  void removeItem(const T &itemToRemove) requires(std::equality_comparable<T>) {
-    std::erase_if(items, [this, &itemToRemove](const auto &item) { return item.first == itemToRemove; });
-    refilterItems();
-  }
-  /**
-   * Remove all items for which predicate returns true.
-   * @param predicate predicate for item removal
-   */
-  void removeItemIf(std::predicate<const T &> auto &&predicate) {
-    std::erase_if(items,
-                  [p = std::forward<decltype(predicate)>(predicate)](const auto &item) { return p(item.first); });
-    refilterItems();
-  }
-  /**
-   * Set filter by which shown items are filtered.
-   * @param filterFnc
-   */
-  void setFilter(std::predicate<const T &> auto filterFnc) {
-    filter = filterFnc;
-    refilterItems();
-  }
-  /**
-   * Remove filter.
-   */
-  void clearFilter() {
-    filter = [](auto) { return true; };
-    refilterItems();
-  }
-  /**
-   * Get reference to all items.
-   * @return references to all items
-   */
-  [[nodiscard]] auto getItems() {
-    return items | std::views::transform([](auto &item) -> T & { return item.first; });
-  }
-  /**
-   * Get reference to all items.
-   * @return references to all items
-   */
-  [[nodiscard]] auto getItems() const {
-    return items | std::views::transform([](auto &item) -> const T & { return item.first; });
-  }
+      : CustomItemBox<T, R>(elementName, label, std::forward<decltype(rowFactory)>(rowFactory)), Resizable(s) {}
 
  protected:
-  virtual void refilterItems() {
-    filteredItems = items | std::views::filter([this](auto &item) { return filter(item.first); })
-        | std::views::transform([](auto &item) { return &item; }) | ranges::to_vector;
-  }
-
   void renderImpl() override {
     if (ImGui::BeginListBox(getLabel().c_str(), getSize().asImVec())) {
-      std::ranges::for_each(filteredItems, [](auto &item) { item->second->render(); });
+      std::ranges::for_each(CustomListboxRowFactory<T, R>::filteredItems, [](auto &item) { item->second->render(); });
       ImGui::EndListBox();
     }
   }
-
-  using Item = std::pair<T, std::unique_ptr<R>>;
-  std::vector<Item> items;
-  std::vector<Item *> filteredItems;
-  std::function<std::unique_ptr<R>(const T &)> factory;
-  std::function<bool(const T &)> filter = [](const auto &) { return true; };
 };
 }// namespace pf::ui::ig
 
