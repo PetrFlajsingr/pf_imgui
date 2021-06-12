ddfa3890f78b7e252903aed056e8ad091b7a7c5e
petr <xflajs00@stud.fit.vutbr.cz>
2021-06-12 20:51:43 +0200

    add for awesome

--- src/pf_imgui/icons.h
+++ src/pf_imgui/icons.h
@@ -6,14 +6,16 @@
 #define PF_IMGUI_SRC_PF_IMGUI_ICONS_H
 
 #include <IconsFontAwesome5.h>
+#include <IconsForkAwesome.h>
 #include <filesystem>
 
 namespace pf::ui::ig {
-enum class IconPack { FontAwesome5 };
+enum class IconPack { FontAwesome5 = 1 << 0, ForkAwesome = 1 << 1 };
 
-std::vector<std::filesystem::path> fontFileNamesForIconPack(IconPack iconPack) {
+inline std::vector<std::filesystem::path> fontFileNamesForIconPack(IconPack iconPack) {
   switch (iconPack) {
-    case IconPack::FontAwesome5: return {FONT_ICON_FILE_NAME_FAR, FONT_ICON_FILE_NAME_FAS};
+    case IconPack::FontAwesome5: return {FONT_ICON_FILE_NAME_FAR};
+    case IconPack::ForkAwesome: return {FONT_ICON_FILE_NAME_FK};
   }
   return {};
 }
@@ -23,13 +25,19 @@ struct IconFontConfig {
   std::vector<ImWchar> iconRange;
 };
 
-IconFontConfig fontConfigForIconPack(IconPack iconPack) {
+inline IconFontConfig fontConfigForIconPack(IconPack iconPack) {
   switch (iconPack) {
     case IconPack::FontAwesome5: {
       auto icons_config = ImFontConfig{};
       icons_config.MergeMode = true;
       icons_config.PixelSnapH = true;
       return {icons_config, {ICON_MIN_FA, ICON_MAX_FA, 0}};
+    }
+    case IconPack::ForkAwesome: {
+      auto icons_config = ImFontConfig{};
+      icons_config.MergeMode = true;
+      icons_config.PixelSnapH = true;
+      return {icons_config, {ICON_MIN_FK, ICON_MIN_FK, 0}};
     }
   }
   return {};
