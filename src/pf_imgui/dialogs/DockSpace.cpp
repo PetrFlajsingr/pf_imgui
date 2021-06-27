804c01398ebf599b5be6c8e8ad08c92e12cec1b0
petr <xflajs00@stud.fit.vutbr.cz>
2021-06-27 13:15:30 +0200

    add DockSpace

---
+++ src/pf_imgui/dialogs/DockSpace.cpp
@@ -0,0 +1,7 @@
+//
+// Created by petr on 6/27/21.
+//
+
+#include "DockSpace.h"
+
+namespace pf::ui::ig{}
---
+++ src/pf_imgui/dialogs/DockSpace.h
@@ -0,0 +1,12 @@
+//
+// Created by petr on 6/27/21.
+//
+
+#ifndef PF_IMGUI_SRC_PF_IMGUI_DIALOGS_DOCKSPACE_H
+#define PF_IMGUI_SRC_PF_IMGUI_DIALOGS_DOCKSPACE_H
+
+namespace pf::ui::ig{
+class DockSpace {};
+}
+
+#endif//PF_IMGUI_SRC_PF_IMGUI_DIALOGS_DOCKSPACE_H
