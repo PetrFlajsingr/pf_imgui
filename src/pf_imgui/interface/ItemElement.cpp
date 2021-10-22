37cf84a10bb1407b9af0719e9e1c0e60842f61ce
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2021-10-22 14:32:08 +0200

    msvc internal errors

--- src/pf_imgui/interface/ItemElement.cpp
+++ src/pf_imgui/interface/ItemElement.cpp
@@ -52,7 +52,9 @@ void ItemElement::setFocus() {
 }
 
 Tooltip &ItemElement::getTooltip() {
+#ifndef _MSC_VER // TODO: MSVC internal error
   if (tooltip == nullptr) { throw StackTraceException("Tooltip doesn't exist in {}", getName()); }
+#endif
   return *tooltip;
 }
 
@@ -64,7 +66,9 @@ PopupMenu &ItemElement::createPopupMenu() {
 }
 
 PopupMenu &ItemElement::getPopupMenu() {
+#ifndef _MSC_VER // TODO: MSVC internal error
   if (tooltip == nullptr) { throw StackTraceException("Popup menu doesn't exist in {}", getName()); }
+#endif
   return *popupMenu;
 }
 
