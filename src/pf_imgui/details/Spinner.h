6ff4d46ae763d609f5a39bdc88c533c4fe538f20
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2021-11-11 14:51:18 +0100

    add Spinner impl

---
+++ src/pf_imgui/details/Spinner.h
@@ -0,0 +1,16 @@
+//
+// Created by xflajs00 on 11.11.2021.
+//
+
+#ifndef PF_IMGUI_SPINNER_H
+#define PF_IMGUI_SPINNER_H
+
+#include <imgui.h>
+
+namespace ImGui {
+
+void Spinner(const char *label, float radius, int thickness, const ImU32 &color);
+
+}
+
+#endif//PF_IMGUI_SPINNER_H
