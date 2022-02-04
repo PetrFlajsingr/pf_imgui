e9e6f60e7b4df56e94d7e4c3d823663d8fb5f9e5
PetrFlajsingr <flajsingr@gmail.com>
2022-02-04 16:06:50 +0100

    replace unordered_map in Observable_impl with vector

--- src/pf_imgui/interface/Observable_impl.h
+++ src/pf_imgui/interface/Observable_impl.h
@@ -1,9 +1,9 @@
 /**
- * @file Observable_impl.h
- * @brief Implementation of observable.
- * @author Petr Flajšingr
- * @date 23.1.21
- */
+* @file Observable_impl.h
+* @brief Implementation of observable.
+* @author Petr Flajšingr
+* @date 23.1.21
+*/
 
 #ifndef PF_IMGUI_INTERFACE_OBSERVABLE_IMPL_H
 #define PF_IMGUI_INTERFACE_OBSERVABLE_IMPL_H
@@ -13,16 +13,16 @@
 #include <pf_common/Subscription.h>
 #include <pf_imgui/_export.h>
 #include <ranges>
-#include <unordered_map>
 #include <utility>
+#include <vector>
 
 namespace pf::ui::ig {
 /**
- * @brief Implementation of observable design pattern.
- *
- * Allows multiple observers, each of which has a unique ID.
- * @tparam Args Types of arguments to be used as callback parameters
- */
+* @brief Implementation of observable design pattern.
+*
+* Allows multiple observers, each of which has a unique ID.
+* @tparam Args Types of arguments to be used as callback parameters
+*/
 template<typename... Args>
 class PF_IMGUI_EXPORT Observable_impl {
  public:
@@ -30,8 +30,7 @@ class PF_IMGUI_EXPORT Observable_impl {
   Observable_impl(const Observable_impl &) = delete;
   Observable_impl &operator=(const Observable_impl &) = delete;
 
-  Observable_impl(Observable_impl &&other) noexcept
-      : listeners(std::move(other.listeners)), idCounter(idCounter) {}
+  Observable_impl(Observable_impl &&other) noexcept : listeners(std::move(other.listeners)), idCounter(idCounter) {}
   Observable_impl &operator=(Observable_impl &&rhs) noexcept {
     listeners = std::move(rhs.listeners);
     idCounter = rhs.idCounter;
@@ -41,24 +40,24 @@ class PF_IMGUI_EXPORT Observable_impl {
   virtual ~Observable_impl() { *exists = false; }
 
   /**
-   * Add a listener and return a Subscription object which can be used to unregister it.
-   * @param fnc listener
-   * @return Subscription for unregistration purposes
-   * @see Subscription
-   */
+  * Add a listener and return a Subscription object which can be used to unregister it.
+  * @param fnc listener
+  * @return Subscription for unregistration purposes
+  * @see Subscription
+  */
   Subscription addListener(std::invocable<const Args &...> auto &&fnc) {
     const auto id = generateListenerId();
-    listeners[id] = std::forward<decltype(fnc)>(fnc);
+    listeners.emplace_back(id, std::forward<decltype(fnc)>(fnc));
     return Subscription([id, this, observableExists = exists] {
       if (!*observableExists) { return; }
-      listeners.erase(id);
+      listeners.erase(std::ranges::find(listeners, id, &ListenerRecord::first));
     });
   }
 
   /**
-   * Notify all listeners with provided parameters.
-   * @param args parameters to be passed to listeners
-   */
+  * Notify all listeners with provided parameters.
+  * @param args parameters to be passed to listeners
+  */
   void notify(const Args &...args) {
     auto callables = listeners | std::views::values;
     std::ranges::for_each(callables, [&](const auto &callable) { callable(args...); });
@@ -67,9 +66,10 @@ class PF_IMGUI_EXPORT Observable_impl {
  private:
   using ListenerId = uint32_t;
   using Callback = std::function<void(const Args &...)>;
+  using ListenerRecord = std::pair<ListenerId, Callback>;
   ListenerId generateListenerId() { return idCounter++; }
 
-  std::unordered_map<ListenerId, Callback> listeners{};
+  std::vector<ListenerRecord> listeners{};
   ListenerId idCounter{};
   std::shared_ptr<bool> exists = std::make_shared<bool>(true);
 };
