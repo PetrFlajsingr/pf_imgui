bd435d60435be16c6103fdf334bb0433d52f0619
PetrFlajsingr <flajsingr@gmail.com>
2022-02-09 11:33:05 +0100

    add Toggle

---
+++ src/pf_imgui/elements/Toggle.cpp
@@ -0,0 +1,7 @@
+//
+// Created by petr.flajsingr on 2/9/2022.
+//
+
+#include "Toggle.h"
+
+namespace pf::ui::ig {}
---
+++ src/pf_imgui/elements/Toggle.h
@@ -0,0 +1,14 @@
+//
+// Created by petr.flajsingr on 2/9/2022.
+//
+
+#ifndef PF_IMGUI_TOGGLE_H
+#define PF_IMGUI_TOGGLE_H
+
+namespace pf::ui::ig {
+
+class Toggle {};
+
+}
+
+#endif  //PF_IMGUI_TOGGLE_H
