5dbdf8e057461185806a886ea3777750bbae17b0
PetrFlajsingr <flajsingr@gmail.com>
2022-02-14 09:04:42 +0100

    proper child size calculation for `WrapLayout`

--- src/pf_imgui/layouts/WrapLayout.cpp
+++ src/pf_imgui/layouts/WrapLayout.cpp
@@ -25,6 +25,11 @@ void WrapLayout::renderImpl() {
       ImGui::SetScrollHereY(0.0f);
       nextFrameScrollPosition = std::nullopt;
     }
+    if (dimensionPreviousFrame.size() != getChildren().size()) {
+      dimensionPreviousFrame.clear();
+      dimensionPreviousFrame.resize(getChildren().size());
+      dimensionsCalculated = false;
+    }
     if (renderCollapseButton()) {
       switch (getLayoutDirection()) {
         case LayoutDirection::LeftToRight: renderLeftToRight(); break;
@@ -41,17 +46,24 @@ void WrapLayout::renderLeftToRight() {
   auto childrenView = getChildren();
   if (!childrenView.empty()) {
     const auto childrenSize = childrenView.size();
-    auto idx = 0u;
+    std::size_t idx{};
     const auto windowVisibleX = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
     const auto &style = ImGui::GetStyle();
     std::ranges::for_each(childrenView, [&](auto &child) {
       child.render();
       const auto lastOriginX = ImGui::GetItemRectMin().x;
       const auto lastX = ImGui::GetItemRectMax().x;
-      const auto nextX = lastX + style.ItemSpacing.x + (lastX - lastOriginX);
-      if (idx != childrenSize - 1 && nextX < windowVisibleX) { ImGui::SameLine(); }
+      if (dimensionsCalculated && idx != childrenSize - 1) {
+        const auto nextXH = lastX + style.ItemSpacing.x + dimensionPreviousFrame[idx + 1];
+        if (idx != childrenSize - 1 && nextXH < windowVisibleX) { ImGui::SameLine(); }
+      } else {
+        const auto nextX = lastX + style.ItemSpacing.x + (lastX - lastOriginX);
+        if (idx != childrenSize - 1 && nextX < windowVisibleX) { ImGui::SameLine(); }
+      }
+      dimensionPreviousFrame[idx] = ImGui::GetItemRectSize().x;
       ++idx;
     });
+    dimensionsCalculated = true;
   }
 }
 
@@ -62,17 +74,28 @@ void WrapLayout::renderTopToBottom() {
     float maxXInColumn = currentX;
     const auto windowVisibleY = ImGui::GetWindowPos().y + ImGui::GetWindowContentRegionMax().y;
     const auto &style = ImGui::GetStyle();
+    std::size_t idx{};
     std::ranges::for_each(childrenView, [&](auto &child) {
       ImGui::SetCursorPosX(currentX);
       child.render();
       maxXInColumn = std::max(maxXInColumn, ImGui::GetItemRectMax().x - ImGui::GetWindowPos().x);
       const auto lastOriginY = ImGui::GetItemRectMin().y;
       const auto lastY = ImGui::GetItemRectMax().y;
-      const auto nextY = lastY + style.ItemSpacing.y + (lastY - lastOriginY);
-      if (nextY >= windowVisibleY) {
-        currentX = maxXInColumn + style.ItemSpacing.x;
-        ImGui::SetCursorPosY(0);
+      if (dimensionsCalculated) {
+        const auto nextYH = lastY + dimensionPreviousFrame[idx + 1];
+        if (nextYH >= windowVisibleY) {
+          currentX = maxXInColumn + style.ItemSpacing.x;
+          ImGui::SetCursorPosY(0);
+        }
+      } else {
+        const auto nextY = lastY + style.ItemSpacing.y + (lastY - lastOriginY);
+        if (nextY >= windowVisibleY) {
+          currentX = maxXInColumn + style.ItemSpacing.x;
+          ImGui::SetCursorPosY(0);
+        }
       }
+      dimensionPreviousFrame[idx] = ImGui::GetItemRectSize().y;
+      ++idx;
     });
   }
 }
--- src/pf_imgui/layouts/WrapLayout.h
+++ src/pf_imgui/layouts/WrapLayout.h
@@ -40,6 +40,59 @@ class WrapLayout : public BoxLayout {
  private:
   void renderLeftToRight();
   void renderTopToBottom();
+
+  std::vector<float> dimensionPreviousFrame{};
+  bool dimensionsCalculated = false;
+};
+
+}  // namespace pf::ui::ig
+
+#endif  //PF_IMGUI_WRAPLAYOUT_H
+//
+// Created by petr.flajsingr on 2/9/2022.
+//
+
+#ifndef PF_IMGUI_WRAPLAYOUT_H
+#define PF_IMGUI_WRAPLAYOUT_H
+
+#include "BoxLayout.h"
+#include <string>
+
+namespace pf::ui::ig {
+
+class WrapLayout : public BoxLayout {
+ public:
+  /**
+   * @brief Struct for construction of WrapLayout.
+   */
+  struct Config {
+    using Parent = WrapLayout;
+    std::string_view name;                           /*!< Unique name of the element */
+    LayoutDirection layoutDirection;                 /*!< Direction the element are rendered in */
+    Size size;                                       /*!< Size of the element */
+    AllowCollapse allowCollapse = AllowCollapse::No; /*!< Allow collapse functionality */
+    ShowBorder showBorder = ShowBorder::No;          /*!< Render border around layout's area */
+    Persistent persistent = Persistent::No;          /*!< Allow state saving to disk */
+  };
+  /**
+   * Construct WrapLayout
+   * @param config construction args @see WrapLayout::Config
+   */
+  explicit WrapLayout(Config &&config);
+
+  WrapLayout(const std::string &elementName, LayoutDirection layoutDirection, const Size &size,
+             AllowCollapse allowCollapse = AllowCollapse::No, ShowBorder showBorder = ShowBorder::No,
+             Persistent persistent = Persistent::No);
+
+ protected:
+  void renderImpl() override;
+
+ private:
+  void renderLeftToRight();
+  void renderTopToBottom();
+
+  std::vector<float> dimensionPreviousFrame{};
+  bool dimensionsCalculated = false;
 };
 
 }  // namespace pf::ui::ig
