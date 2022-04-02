6737568eb97a5c0d311c6a3bb0d7f70a51b1bdc2
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2022-04-02 22:23:58 +0200

    add basic `SimpleCurveEditor` implementation

--- src/pf_imgui/elements/SimpleCurveEditor.cpp
+++ src/pf_imgui/elements/SimpleCurveEditor.cpp
@@ -4,4 +4,29 @@
 
 #include "SimpleCurveEditor.h"
 
-namespace pf::ui::ig {}
+namespace pf::ui::ig {
+
+SimpleCurveEditor::SimpleCurveEditor(const std::string &name, Size s, const std::string &label,
+                                     std::size_t maxPointCount)
+    : Element(name), Resizable(s), Labellable(label), ValueObservable(getViewToCurveData()),
+      curveData(maxPointCount + 1, ImVec2{0, 0}) {}
+
+void SimpleCurveEditor::setMaxPointCount(std::size_t count) { curveData.resize(count + 1); }
+
+std::size_t SimpleCurveEditor::getMaxPointCount() const { return curveData.size() - 1; }
+
+void SimpleCurveEditor::renderImpl() {
+  auto styleRAII = setStyleStack();
+  auto colorRAII = setColorStack();
+  if (ImGui::Curve(getLabel().c_str(), static_cast<ImVec2>(getSize()), static_cast<int>(getMaxPointCount()),
+                   curveData.data())) {
+    setValueInner(getViewToCurveData());
+    notifyValueChanged();
+  }
+}
+
+CurvePointsView SimpleCurveEditor::getViewToCurveData() const {
+  return curveData | ranges::views::take_while(details::IsValidCurvePoint{});
+}
+
+}  // namespace pf::ui::ig
--- src/pf_imgui/elements/SimpleCurveEditor.h
+++ src/pf_imgui/elements/SimpleCurveEditor.h
@@ -8,9 +8,52 @@
 #ifndef PF_IMGUI_SIMPLECURVEEDITOR_H
 #define PF_IMGUI_SIMPLECURVEEDITOR_H
 
+#include <ImGuiCurveEditor.h>
+#include <pf_imgui/interface/Customizable.h>
+#include <pf_imgui/interface/Element.h>
+#include <pf_imgui/interface/Labellable.h>
+#include <pf_imgui/interface/Resizable.h>
+#include <pf_imgui/interface/ValueObservable.h>
+#include <range/v3/view/take_while.hpp>
+#include <range/v3/view/zip.hpp>
+
 namespace pf::ui::ig {
+namespace details {
+struct IsValidCurvePoint {
+  bool operator()(const ImVec2 &point) { return point.x != -1; }
+};
+}  // namespace details
 
-class SimpleCurveEditor {};
+using CurvePointsView =
+    ranges::take_while_view<ranges::ref_view<const std::vector<ImVec2>>, details::IsValidCurvePoint>;
+struct CurvePointsViewComparator {
+  [[nodiscard]] bool operator()(CurvePointsView lhs, CurvePointsView rhs) {
+    return std::ranges::all_of(ranges::views::zip(lhs, rhs), [](const auto &val) {
+      const auto &[lhs, rhs] = val;
+      return lhs.x == rhs.x && lhs.y == rhs.y;
+    });
+  }
+};
 
-}
+class SimpleCurveEditor : public Element,
+                          public Resizable,
+                          public Labellable,
+                          public ValueObservable<CurvePointsView, CurvePointsViewComparator>,
+                          public AllColorCustomizable,
+                          public AllStyleCustomizable {
+ public:
+  SimpleCurveEditor(const std::string &name, Size s, const std::string &label, std::size_t maxPointCount);
+
+  void setMaxPointCount(std::size_t count);
+  [[nodiscard]] std::size_t getMaxPointCount() const;
+
+ protected:
+  void renderImpl() override;
+
+ private:
+  [[nodiscard]] CurvePointsView getViewToCurveData() const;
+  std::vector<ImVec2> curveData;
+};
+
+}  // namespace pf::ui::ig
 #endif  //PF_IMGUI_SIMPLECURVEEDITOR_H
