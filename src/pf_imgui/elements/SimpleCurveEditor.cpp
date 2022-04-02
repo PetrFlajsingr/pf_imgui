aac535efd3f39e524ebda6d111737a6a4ef13b91
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2022-04-02 22:17:47 +0200

    add `SimpleCurveEditor`

---
+++ src/pf_imgui/elements/SimpleCurveEditor.cpp
@@ -0,0 +1,7 @@
+//
+// Created by xflajs00 on 02.04.2022.
+//
+
+#include "SimpleCurveEditor.h"
+
+namespace pf::ui::ig {}
---
+++ src/pf_imgui/elements/SimpleCurveEditor.h
@@ -0,0 +1,16 @@
+/**
+* @file SimpleCurveEditor.h
+* @brief A simple smoothing curve editor.
+* @author Petr Flajšingr
+* @date 2.4.22
+*/
+
+#ifndef PF_IMGUI_SIMPLECURVEEDITOR_H
+#define PF_IMGUI_SIMPLECURVEEDITOR_H
+
+namespace pf::ui::ig {
+
+class SimpleCurveEditor {};
+
+}
+#endif  //PF_IMGUI_SIMPLECURVEEDITOR_H
