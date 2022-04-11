7b72eb6bee34c20b335f4b5aa12d5d48b0498ebc
PetrFlajsingr <flajsingr@gmail.com>
2022-04-11 19:50:18 +0200

    add simple `TimePicker`

---
+++ src/pf_imgui/elements/TimePicker.cpp
@@ -0,0 +1,98 @@
+//
+// Created by xflajs00 on 11.04.2022.
+//
+
+#include "TimePicker.h"
+
+namespace pf::ui::ig {
+
+TimePicker::TimePicker(TimePicker::Config &&config)
+    : TimePicker(std::string{config.name}, config.label, config.value, config.persistent) {}
+
+TimePicker::TimePicker(const std::string &name, const std::string &label, pf::ui::ig::TimeOfDay value,
+                       pf::ui::ig::Persistent persistent)
+    : Element(name), Labellable(label), ValueObservable(value), Savable(persistent),
+      spinH("spinH", "", 0, 23, static_cast<int>(duration_cast<std::chrono::hours>(value.to_duration()).count())),
+      spinM("spinM", "", 0, 59,
+            static_cast<int>(duration_cast<std::chrono::minutes>(value.to_duration()).count() % 60)),
+      spinS("spinS", "", 0, 59,
+            static_cast<int>(duration_cast<std::chrono::seconds>(value.to_duration()).count() % 60)) {
+  spinH.addValueListener([this](auto hours) {
+    const auto time = getValue().to_duration();
+    const auto timeMmSs = time - duration_cast<std::chrono::hours>(time);
+    const auto newTime = TimeOfDay{std::chrono::hh_mm_ss{timeMmSs + std::chrono::hours{hours}}};
+    setValueAndNotifyIfChanged(newTime);
+  });
+  spinM.addValueListener([this](auto minutes) {
+    const auto time = getValue().to_duration();
+    const auto timeHhSs = time - duration_cast<std::chrono::minutes>(time) % 60;
+    const auto newTime = TimeOfDay{std::chrono::hh_mm_ss{timeHhSs + std::chrono::minutes{minutes}}};
+    setValueAndNotifyIfChanged(newTime);
+  });
+  spinS.addValueListener([this](auto seconds) {
+    const auto time = getValue().to_duration();
+    const auto timeHhMm = time - duration_cast<std::chrono::seconds>(time) % 60;
+    const auto newTime = TimeOfDay{std::chrono::hh_mm_ss{timeHhMm + std::chrono::seconds{seconds}}};
+    setValueAndNotifyIfChanged(newTime);
+  });
+}
+
+void TimePicker::renderImpl() {
+  {
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{0, 0});
+    pf::RAII popStyleVar{[] { ImGui::PopStyleVar(); }};
+    ImGui::SetNextItemWidth(65);
+    spinH.render();
+    ImGui::SameLine();
+    ImGui::SetNextItemWidth(65);
+    spinM.render();
+    ImGui::SameLine();
+    ImGui::SetNextItemWidth(65);
+    spinS.render();
+  }
+  ImGui::SameLine();
+  ImGui::Text("%s", getLabel().c_str());
+}
+
+void TimePicker::unserialize_impl(const toml::table &src) {
+  using namespace std::chrono;
+  auto partsFound = 0;
+  hours newHours{};
+  minutes newMinutes{};
+  seconds newSeconds{};
+  if (auto yearIter = src.find("hours"); yearIter != src.end()) {
+    if (auto newHoursVal = yearIter->second.as_integer(); newHoursVal != nullptr) {
+      newHours = hours{static_cast<unsigned int>(newHoursVal->get())};
+      ++partsFound;
+    }
+  }
+  if (auto monthIter = src.find("minutes"); monthIter != src.end()) {
+    if (auto newMinutesVal = monthIter->second.as_integer(); newMinutesVal != nullptr) {
+      newMinutes = minutes{static_cast<unsigned int>(newMinutesVal->get())};
+      ++partsFound;
+    }
+  }
+  if (auto dayIter = src.find("seconds"); dayIter != src.end()) {
+    if (auto newSecondsVal = dayIter->second.as_integer(); newSecondsVal != nullptr) {
+      newSeconds = seconds{static_cast<unsigned int>(newSecondsVal->get())};
+      ++partsFound;
+    }
+  }
+
+  if (partsFound == 3) { setValue(TimeOfDay{hh_mm_ss<seconds>{newHours + newMinutes + newSeconds}}); }
+}
+
+toml::table TimePicker::serialize_impl() const {
+  return toml::table{{"hours", getValue().hours().count()},
+                     {"minutes", getValue().minutes().count() % 60},
+                     {"seconds", getValue().seconds().count() % 60}};
+}
+
+void TimePicker::setValue(const pf::ui::ig::TimeOfDay &newValue) {
+  spinH.setValue(static_cast<int>(duration_cast<std::chrono::hours>(newValue.to_duration()).count()));
+  spinM.setValue(static_cast<int>(duration_cast<std::chrono::minutes>(newValue.to_duration()).count() % 60));
+  spinS.setValue(static_cast<int>(duration_cast<std::chrono::seconds>(newValue.to_duration()).count() % 60));
+  ValueObservable::setValue(newValue);
+}
+
+}  // namespace pf::ui::ig
---
+++ src/pf_imgui/elements/TimePicker.h
@@ -0,0 +1,79 @@
+/**
+* @file TimePicker.h
+* @brief TimePicker element.
+* @author Petr Flajšingr
+* @date 11.4.22
+*/
+
+#ifndef PF_IMGUI_ELEMENTS_TIMEPICKER_H
+#define PF_IMGUI_ELEMENTS_TIMEPICKER_H
+
+#include <chrono>
+#include <pf_imgui/elements/SpinInput.h>
+#include <pf_imgui/interface/Element.h>
+#include <pf_imgui/interface/Labellable.h>
+#include <pf_imgui/interface/Savable.h>
+#include <pf_imgui/interface/ValueObservable.h>
+
+namespace pf::ui::ig {
+
+using TimeOfDay = std::chrono::hh_mm_ss<std::chrono::seconds>;
+namespace details {
+struct TimeOfDayComparator {
+  [[nodiscard]] inline bool operator()(TimeOfDay lhs, TimeOfDay rhs) const {
+    return lhs.to_duration() == rhs.to_duration();
+  }
+};
+}  // namespace details
+
+/**
+ * @brief Simple HH MM SS 24 time picker.
+ */
+class TimePicker : public Element,
+                   public Labellable,
+                   public ValueObservable<TimeOfDay, details::TimeOfDayComparator>,
+                   public Savable {
+ public:
+  /**
+   * @brief Construction config for TimePicker.
+   */
+  struct Config {
+    using Parent = TimePicker;
+    std::string_view name;                  /*!< Unique name of the element */
+    std::string label;                      /*!< Text rendered next to the input */
+    TimeOfDay value{};                      /*!< Initial value */
+    Persistent persistent = Persistent::No; /*!< Enable disk state saving */
+  };
+  /**
+   * Construct TimePicker
+   * @param config construction args @see TimePicker::Config
+   */
+  explicit TimePicker(Config &&config);
+  /**
+   * Construct TimePicker
+   * @param name unique name of the element
+   * @param label text rendered next to the input
+   * @param value initial value
+   * @param persistent enable disk state saving
+   */
+  TimePicker(const std::string &name, const std::string &label, TimeOfDay value,
+             Persistent persistent = Persistent::No);
+
+  void setValue(const TimeOfDay &newValue) override;
+
+ protected:
+  void renderImpl() override;
+
+  void unserialize_impl(const toml::table &src) override;
+
+  [[nodiscard]] toml::table serialize_impl() const override;
+
+ private:
+  SpinInput<int> spinH;
+  SpinInput<int> spinM;
+  SpinInput<int> spinS;
+};
+
+}  // namespace pf::ui::ig
+
+#endif  //PF_IMGUI_ELEMENTS_TIMEPICKER_H
