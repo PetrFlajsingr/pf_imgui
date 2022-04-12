70c04e3d43883c1385f166b4101ecc322eeee355
PetrFlajsingr <flajsingr@gmail.com>
2022-04-12 20:31:00 +0200

    replace binary enum usage in 'Checkbox' config with bool

--- src/pf_imgui/elements/Checkbox.cpp
+++ src/pf_imgui/elements/Checkbox.cpp
@@ -10,7 +10,7 @@ namespace pf::ui::ig {
 
 Checkbox::Checkbox(Checkbox::Config &&config)
     : ItemElement(std::string{config.name}), ValueObservable(config.checked), Labellable(std::string{config.label}),
-      Savable(config.persistent) {}
+      Savable(config.persistent ? Persistent::Yes : Persistent::No) {}
 
 Checkbox::Checkbox(const std::string &elementName, const std::string &label, bool value, Persistent persistent)
     : ItemElement(elementName), ValueObservable(value), Labellable(label), Savable(persistent) {}
