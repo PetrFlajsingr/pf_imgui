c59536c9bef744bed0d0f69176a899b22fb2b222
PetrFlajsingr <flajsingr@gmail.com>
2022-04-23 21:34:11 +0200

    Fixed position and size modification for `ModalDialog`

--- src/pf_imgui/dialogs/ModalDialog.cpp
+++ src/pf_imgui/dialogs/ModalDialog.cpp
@@ -19,6 +19,14 @@ void ModalDialog::renderImpl() {
   [[maybe_unused]] auto colorStyle = setColorStack();
   [[maybe_unused]] auto style = setStyleStack();
   if (font != nullptr) { ImGui::PushFont(font); }
+  if (sizeDirty) {
+    sizeDirty = false;
+    ImGui::SetNextWindowSize(static_cast<ImVec2>(getSize()));
+  }
+  if (positionDirty) {
+    positionDirty = false;
+    ImGui::SetNextWindowPos(static_cast<ImVec2>(getPosition()));
+  }
   if (firstRender) { ImGui::OpenPopup(getLabel().c_str()); }
   if (ImGui::BeginPopupModal(getLabel().c_str())) {
     RAII endPopup{ImGui::EndPopup};
@@ -35,13 +43,15 @@ void ModalDialog::close() { closed = true; }
 bool ModalDialog::isClosed() const { return closed; }
 
 void ModalDialog::setSize(const Size &newSize) {
-  Resizable::setSize(newSize);  // FIXME change this to SetNextWindowSize
-  ImGui::SetWindowSize(getLabel().c_str(), static_cast<ImVec2>(getSize()));
+  sizeDirty = true;
+  Resizable::setSize(newSize);
 }
+
 void ModalDialog::setPosition(Position pos) {
-  ImGui::SetWindowPos(getLabel().c_str(), static_cast<ImVec2>(pos));
+  positionDirty = true;
   Positionable::setPosition(pos);
 }
+
 void ModalDialog::setFont(ImFont *fontPtr) { font = fontPtr; }
 
 }  // namespace pf::ui::ig
