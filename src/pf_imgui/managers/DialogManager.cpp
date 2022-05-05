c4c949e8b60a5172aaefcda652819d645fb30629
PetrFlajsingr <flajsingr@gmail.com>
2022-05-05 23:10:35 +0200

    Added `DialogManager`

---
+++ src/pf_imgui/managers/DialogManager.cpp
@@ -0,0 +1,9 @@
+//
+// Created by xflajs00 on 05.05.2022.
+//
+
+#include "DialogManager.h"
+
+namespace pf::ui::ig {
+
+}
---
+++ src/pf_imgui/managers/DialogManager.h
@@ -0,0 +1,21 @@
+//
+// Created by xflajs00 on 05.05.2022.
+//
+
+#ifndef IMGUI_EXPERIMENTS_DIALOGMANAGER_H
+#define IMGUI_EXPERIMENTS_DIALOGMANAGER_H
+
+#include <pf_imgui/_export.h>
+
+namespace pf::ui::ig {
+
+class PF_IMGUI_EXPORT DialogManager {
+ public:
+
+ private:
+
+};
+
+}
+
+#endif  //IMGUI_EXPERIMENTS_DIALOGMANAGER_H
