56022bbee3053701dfbf0d2d20995fea18d6271c
PetrFlajsingr <flajsingr@gmail.com>
2022-05-09 20:54:25 +0200

    Removed dead code

--- src/pf_imgui/elements/Image.cpp
+++ src/pf_imgui/elements/Image.cpp
@@ -25,30 +25,9 @@ void Image::renderImpl() {
   } else {
     ImGui::Image(textureId, static_cast<ImVec2>(getSize()), uvStart, uvEnd);
   }
-  // if (pixelInspectionTooltipEnabled) { // TODO: implement in derived
-  //   auto &io = ImGui::GetIO();
-  //   auto rc = ImRect(ImGui::GetItemRectMin(), ImGui::GetItemRectMax());
-  //   ImVec2 mouseUVCoord = (io.MousePos - rc.Min) / rc.GetSize();
-  //   mouseUVCoord.y = 1.f - mouseUVCoord.y;
-  //
-  //   if (io.KeyShift && io.MouseDown[0] && mouseUVCoord.x >= 0.f && mouseUVCoord.y >= 0.f) {
-  //     int width = getSize().width;
-  //     int height = getSize().height;
-  //
-  //     imageInspect(width, height, pickerImage.GetBits(), mouseUVCoord, displayedTextureSize);
-  //   }
-  // }
 }
 
 void Image::setTextureId(ImTextureID imTextureId) { textureId = imTextureId; }
 bool Image::isButton() const { return isButton_; }
-
-// bool Image::isPixelInspectionTooltipEnabled() const {
-//   return pixelInspectionTooltipEnabled;
-// }
-//
-// void Image::setPixelInspectionTooltipEnabled(bool enabled) {
-//   pixelInspectionTooltipEnabled = enabled;
-// }
 
 }  // namespace pf::ui::ig
