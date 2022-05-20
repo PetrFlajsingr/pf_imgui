84c6bbff48bb2c2848ef07c5c0007b11882ce016
PetrFlajsingr <flajsingr@gmail.com>
2022-05-21 00:35:30 +0200

    Added `IndeterminateProgressBar`

---
+++ src/pf_imgui/details/IndeterminateProgressBar.h
@@ -0,0 +1,16 @@
+//
+// Created by xflajs00 on 21.05.2022.
+//
+
+#ifndef PF_IMGUI_DETAIL_INDETERMINATEPGORESSBAR_H
+#define PF_IMGUI_DETAIL_INDETERMINATEPGORESSBAR_H
+
+#include <imgui.h>
+
+namespace ImGui {
+
+void IndeterminateProgressBar(float fraction, const ImVec2 &size_arg, const char *overlay);
+
+}  // namespace ImGui
+
+#endif // PF_IMGUI_DETAIL_INDETERMINATEPGORESSBAR_H
