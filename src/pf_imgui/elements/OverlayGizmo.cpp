3dc71f6e55f959bb7c116c26f459207085b12ff8
PetrFlajsingr <flajsingr@gmail.com>
2022-05-22 12:17:17 +0200

    Added `OverlayGizmo.cpp/h`

---
+++ src/pf_imgui/elements/OverlayGizmo.cpp
@@ -0,0 +1,7 @@
+//
+// Created by xflajs00 on 22.05.2022.
+//
+
+#include "OverlayGizmo.h"
+
+namespace pf::ui::ig {}
---
+++ src/pf_imgui/elements/OverlayGizmo.h
@@ -0,0 +1,14 @@
+//
+// Created by xflajs00 on 22.05.2022.
+//
+
+#ifndef PF_IMGUI_ELEMENTS_OVERLAYGIZMO_H
+#define PF_IMGUI_ELEMENTS_OVERLAYGIZMO_H
+
+namespace pf::ui::ig {
+
+
+
+}  // namespace pf
+
+#endif // PF_IMGUI_ELEMENTS_OVERLAYGIZMO_H
