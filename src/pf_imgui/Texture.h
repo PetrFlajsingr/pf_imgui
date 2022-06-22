ec0126c49db0e913eab2e6b3a80e912f573d629b
PetrFlajsingr <flajsingr@gmail.com>
2022-06-22 19:36:59 +0200

    Added missing `Texture` comments

--- src/pf_imgui/Texture.h
+++ src/pf_imgui/Texture.h
@@ -1,21 +1,27 @@
-//
-// Created by Petr on 6/22/2022.
-//
+/**
+ * @file Texture.h
+ * @brief Type representing a texture in pf_imgui.
+ * @author Petr Flajšingr
+ * @date 22.6.22
+ */
 
 #ifndef PF_IMGUI_TEXTURE_H
 #define PF_IMGUI_TEXTURE_H
 
-#include <pf_imgui/_export.h>
 #include <imconfig.h>
+#include <pf_imgui/_export.h>
 
 namespace pf::ui::ig {
 
+/**
+ * @brief A texture used for image rendering.
+ */
 class PF_IMGUI_EXPORT Texture {
  public:
   virtual ~Texture() = default;
   [[nodiscard]] virtual ImTextureID getID() const = 0;
 };
 
-}
+}  // namespace pf::ui::ig
 
 #endif  //PF_IMGUI_TEXTURE_H
